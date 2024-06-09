b59b9c197f2760fb5519e089ab28a049abab6d5d
ThaisSBarbosa <thssantosbarbosa@gmail.com>
2024-06-09 19:52:27 -0300

    tirando teste do prompt

--- SimplifAI/Utils/PromptHelper.cs
+++ SimplifAI/Utils/PromptHelper.cs
@@ -9,7 +9,7 @@ namespace SimplifAI.Utils
         public static string msgSystem = "Você é um especialista jurídico que escreve de forma simples e acessível, " +
             "facilitando o entendimento de textos legais para pessoas leigas.\n" +
             "Os trechos entre parênteses, que se assemelham a uma definição, devem ser considerados para construir o texto final simplificado," +
-            "com o objetivo de explicar de forma mais simples termos complexos. verifique tres vezes se o texto tem a palavra Termo";
+            "com o objetivo de explicar de forma mais simples termos complexos.";
 
         public static string msgPrompt = @"\n O texto acima está escrito em português e trata-se de um contrato jurídico.
             Você deverá simplificá-lo de forma que uma pessoa leiga consiga entendê-lo com o mínimo de dificuldades, ou seja, deverá conter palavras 
